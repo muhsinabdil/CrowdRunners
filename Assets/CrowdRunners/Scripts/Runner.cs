340d1050023e3d5c72eb610c828c8c49e0d21780
muhsinabdil <muhsinabdil@hotmail.com>
2020-02-10 15:45:48 +0300

    create enemy script and enemy object

---
+++ Assets/CrowdRunners/Scripts/Runner.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour
+{
+
+
+    [Header("Settings")]
+    private bool isTarget;//
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool IsTarget()
+    {
+        return isTarget;//! değeri dönüyoruz
+    }
+
+    public void SetTarget(bool v)
+    {
+        isTarget = v;//! değeri set ediyoruz
+    }
+
+}
